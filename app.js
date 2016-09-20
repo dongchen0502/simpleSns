2d29f177dac3f08c639b9a671ad1de28577cb7dd
dongchen0502 <dongchen0502@163.com>
2016-09-20 23:45:03 +0800

    update app.js

--- app.js
+++ app.js
@@ -1,7 +1,47 @@
 var http = require('http');
+var parseUrl = require('url').parse;
+
+function homeController(req, res){
+	res.end('home\n');
+}
+
+function userController(req, res){
+	res.end('user\n');
+}
+
+function notFountController(req, res){
+	res.writeHead(404);
+	res.end('404\n');
+}
+
+function find(arr, match){
+	for(var i in arr){
+		if(match(arr[i])){
+			return arr[i];
+		}
+	}
+}
+
+const rules = [
+	{
+		path : '/', 
+		controller : homeController
+	},
+	{
+		path : '/user', 
+		controller : userController
+	}
+];
 
 var server = http.createServer(function(req, res){
-	res.end('Hello world\n');
+	var urlInfo = parseUrl(req.url);
+	console.log('url:', req.url, 'urlInfo：', urlInfo);
+
+	var rule = find(rules, function(rule){
+		return rule.path == urlInfo.pathname;
+	});
+	var controller = rule && rule.controller || notFountController
+	controller(req, res);
 });
 
 server.listen(3000);
